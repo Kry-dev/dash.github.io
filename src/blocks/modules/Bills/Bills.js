9ba2c6ade07ce1dbfb2835b856b8d6e94b7e80e1
Kry-dev <yaroslavkryvda@gmail.com>
2021-01-21 20:57:33 +0200

    added Bills tab

---
+++ src/blocks/modules/Bills/Bills.js
@@ -0,0 +1,13 @@
+modules.define('Bills', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
