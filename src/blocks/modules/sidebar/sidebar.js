aac61ec2c74c1705cd2e7d206694d2677ff15a40
Kry-dev <yaroslavkryvda@gmail.com>
2021-01-31 20:32:06 +0200

    add editable jquery to sidebar accordion

--- src/blocks/modules/sidebar/sidebar.js
+++ src/blocks/modules/sidebar/sidebar.js
@@ -1,13 +1,17 @@
-modules.define('sidebar', ['i-bem-dom'], function(provide, bemDom) {
+import $ from "jquery";
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
-
+$(document).ready(function(){
+    $("#sidebar .nav-link").on("click", function (){
+        $("#sidebar .nav-item").removeClass("active");
+        $(this).parent().toggleClass("active");
+    });
+    $(".js-edit").on("click", function(){
+        $(this).find(".fa-pencil-square-o").toggleClass("fa-check-square-o text-success");
+        
+        let form = $(this).parent().find("form");
+        form.toggleClass("is-readonly is-editing");
+        
+        let isReadonly = form.hasClass("is-readonly");
+        form.find("input,textarea").prop("disabled", isReadonly);
+    });
 });
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
+import "%modules%/sidebar/sidebar";
 import "%modules%/Signature/Signature";
 // import "%modules%/modal/modal";
 // import "%modules%/scroll-top/scroll-top";
